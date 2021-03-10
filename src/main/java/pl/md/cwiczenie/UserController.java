74fbd71b7c551458e0f37bfefb998899193fc948
mdec92 <mateuszdec092@gmail.com>
2021-03-10 19:50:51 +0100

    Spring cwiczenie

--- src/main/java/pl/md/cwiczenie/UserController.java
+++ src/main/java/pl/md/cwiczenie/UserController.java
@@ -2,10 +2,7 @@ package pl.md.cwiczenie;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
@@ -23,7 +20,10 @@ public class UserController {
     @GetMapping("/users")
     public String users() {
         List<User> users = userRepository.getAll();
+        return convertToString(users);
+    }
 
+    private String convertToString(List<User> users) {
         String result = "";
 
         for (User user : users) {
@@ -32,19 +32,36 @@ public class UserController {
         return result;
     }
 
-
     @RequestMapping("/add")
     public String add(@RequestParam(defaultValue = "Anonim", required = false) String imie,
                       @RequestParam String nazwisko,
                       @RequestParam Integer wiek) {
-
-        if (StringUtils.isEmpty(imie)) {
+        if(StringUtils.isEmpty(imie)) {
             return "redirect:/err.html";
-        } else {
+        } else  {
             User user = new User(imie, nazwisko, wiek);
             userRepository.add(user);
             return "redirect:/success.html";
         }
+    }
+
+    @PostMapping("/search")
+    @ResponseBody
+    public String search(@RequestParam String imie,
+                         @RequestParam String nazwisko,
+                         @RequestParam Integer wiek) {
+
+        List<User> users = userRepository.search(imie, nazwisko, wiek);
+        return convertToString(users);
+    }
+
+    @PostMapping("/delete")
+    public String delete(@RequestParam String imie,
+                         @RequestParam String nazwisko,
+                         @RequestParam Integer wiek) {
+
+        userRepository.delete(imie, nazwisko, wiek);
+        return "redirect:/users";
     }
 
 }
--- src/main/java/pl/md/cwiczenie/UserRepository.java
+++ src/main/java/pl/md/cwiczenie/UserRepository.java
@@ -1,8 +1,10 @@
 package pl.md.cwiczenie;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 @Repository
@@ -15,6 +17,7 @@ public class UserRepository {
         users.add(new User("Adam", "Kowalski", 20));
         users.add(new User("Joanna", "Marciniak", 26));
         users.add(new User("Tomasz", "Zalewski", 55));
+        users.add(new User("Tomasz", "Tanowski", 45));
     }
 
     public List<User> getAll() {
@@ -24,4 +27,48 @@ public class UserRepository {
     public void add(User user) {
         users.add(user);
     }
+
+    public List<User> search(String imie, String nazwisko, Integer wiek) {
+        List<User> resultList = new LinkedList<>();
+        for (User user : users) {
+            boolean isOk = checkSearchConditionsForUser(imie, nazwisko, wiek, user);
+            if(isOk) {
+                resultList.add(user);
+            }
+        }
+
+        return resultList;
+    }
+
+    private boolean checkSearchConditionsForUser(String imie, String nazwisko, Integer wiek, User user) {
+        boolean nameOk = true;
+        boolean surnameOk = true;
+        boolean ageOk = true;
+
+        if(!StringUtils.isEmpty(imie) && !imie.equals(user.getName())) {
+            nameOk = false;
+        }
+
+        if(!StringUtils.isEmpty(nazwisko) && !nazwisko.equals(user.getLastName())) {
+            surnameOk = false;
+        }
+
+        if(wiek != null && wiek != user.getAge()) {
+            ageOk = false;
+        }
+
+        return nameOk && surnameOk && ageOk;
+    }
+
+    public void delete(String imie, String nazwisko, Integer wiek) {
+        List<User> usersToDelete = new LinkedList<>();
+        for (User user : users) {
+            boolean isOk = checkSearchConditionsForUser(imie, nazwisko, wiek, user);
+            if(isOk) {
+                usersToDelete.add(user);
+            }
+        }
+        users.removeAll(usersToDelete);
+    }
+
 }
